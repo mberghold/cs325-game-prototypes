efd2d74f5b06b71daa62da30aee8d914fd175998
Michael Berghold <mberghol@masonlive.gmu.edu>
2017-02-08 21:30:28 -0500

    Game
    The game mechanics are complete.

--- Asteroid-Defense/js/main.js
+++ Asteroid-Defense/js/main.js
@@ -15,38 +15,76 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
+    var timer = 0;
+    var total = 0;
+    var speed = 1;
+
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'background', 'assets/Galaxy.png' );
+        game.load.image( 'asteroid', 'assets/Cowboy-Asteroid2.png' );
+        game.load.image( 'reticle', 'assets/Cannon-Reticle2.png' );
+        game.load.image( 'cannon', 'assets/NASA-Cannon2.png' );
+        game.load.image( 'blast', 'assets/Cannon-Blast2.png' );
+
     }
-    
-    var bouncy;
-    
+  
+    var cannon
+    var reticle
+    var background
+    var asteroid
+    var speed
+
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        background = game.add.image( 0, 0, 'background');
+        cannon = game.add.sprite( 750, 550, 'cannon');
+        reticle = game.add.sprite( 375, 275, 'reticle');
+        reticle.anchor.setTo(0.5, 0.5);
+        cannon.anchor.setTo(0.5, 0.8);
+        game.physics.enable(reticle, Phaser.Physics.ARCADE);
+        game.physics.enable(cannon, Phaser.Physics.ARCADE);
+        // releaseAsteroid();
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        //screen.anchor.setTo( 0.5, 0.5 );
         
         // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        // game.physics.enable( bouncy, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        // bouncy.body.collideWorldBounds = true;
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+    }
+
+    function releaseAsteroid() {
+        asteroid = game.add.sprite(-(Math.random() * 800), (game.world.randomY%400 + 150), 'asteroid');
+        asteroid.anchor.setTo(0.5, 0.5);
+        asteroid.inputEnabled = true;
+        asteroid.input.useHandCursor = true;
+        asteroid.events.onInputDown.add(destroyAsteroid, this);
+        game.add.tween(asteroid).to({ x: game.width + (1600 + asteroid.x) }, (30000/speed), Phaser.Easing.Linear.None, true);
+        total++;
+        timer = game.time.now + 50;
+    }
+
+    function destroyAsteroid (sprite) {
+        sprite.destroy();
+        total--;
+        speed = speed + .2;
     }
     
     function update() {
+
+        if (total < 5 && game.time.now > timer)
+        {
+           releaseAsteroid();
+        }
         // Accelerate the 'logo' sprite towards the cursor,
         // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        game.physics.arcade.moveToPointer(reticle, 300, this.game.input.activePointer, 60);
+        cannon.rotation = game.physics.arcade.angleToPointer(cannon, this.game.input.activePointer) + 1.6;
     }
 };
