affba76c811d8235beba5dbd972fc2bd8e6a495f
Michael Berghold <mberghol@masonlive.gmu.edu>
2017-04-05 23:56:19 -0400

    update

--- Sequence-Game-One/js/MainMenu.js
+++ Sequence-Game-One/js/MainMenu.js
@@ -2,17 +2,25 @@
 
 GameStates.makeMainMenu = function( game, shared ) {
 
-	var music = null;
 	var playButton = null;
+    var instruct = null;
+    var toggle = true;
     
     function startGame(pointer) {
 
         //	Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
-        music.stop();
 
         //	And start the actual game
         game.state.start('Game');
 
+    }
+
+    function showInstruct() {
+        if(toggle) {
+            instruct = game.add.sprite(0, 0, 'instruct') 
+        } else {
+            instruct.destroy();
+        }
     }
     
     return {
@@ -22,13 +30,12 @@ GameStates.makeMainMenu = function( game, shared ) {
             //	We've already preloaded our assets, so let's kick right into the Main Menu itself.
             //	Here all we're doing is playing some music and adding a picture and button
             //	Naturally I expect you to do something significantly better :)
+            key1 = game.input.keyboard.addKey(Phaser.Keyboard.I);
+            key1.onDown.add(toggleInstruct);
     
-            music = game.add.audio('titleMusic');
-            music.play();
+            game.add.sprite(0, 0, 'menuback');
     
-            game.add.sprite(0, 0, 'titlePage');
-    
-            playButton = game.add.button( 303, 400, 'playButton', startGame, null, 'over', 'out', 'down');
+            playButton = game.add.button( 503, 550, 'menuplay', startGame);
     
         },
     
