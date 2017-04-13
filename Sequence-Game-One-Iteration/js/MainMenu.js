23532dea173c8349ebac26af1872b0d3dfc3708d
Michael Berghold <mberghol@masonlive.gmu.edu>
2017-04-12 23:48:03 -0400

    update

--- Sequence-Game-One-Iteration/js/MainMenu.js
+++ Sequence-Game-One-Iteration/js/MainMenu.js
@@ -38,7 +38,7 @@ GameStates.makeMainMenu = function( game, shared ) {
     
             game.add.sprite(0, 0, 'menuback');
 
-            console.log("Update 0.0");
+            console.log("Update 0.1");
     
             playButton = game.add.button( 503, 550, 'play', startGame);
     
